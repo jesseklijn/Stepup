68382fdd56e1999b4238d719101328abc307863b
DESKTOP-T68U191\Gebruiker <jesse.klijn@hva.nl>
2017-12-08 20:55:53 +0900

    Basic Prototype

--- UnityProject/Assets/Scripts/RailwaySystem/CameraMovement.cs
+++ UnityProject/Assets/Scripts/RailwaySystem/CameraMovement.cs
@@ -8,31 +8,44 @@ public class CameraMovement : MonoBehaviour
 
     public Movement movement;
 
-    public IEnumerator MoveTowardsClosestWayPoint(Vector3 PointA, Vector3 PointB, float time, GameObject toMove)
+    private void Start()
     {
-        //Check if character is already moving..
-        //Debug.Log(PointA + " # " + PointB);
-        if (movement.isMoving == false)
+
+    }
+
+    public IEnumerator Accelerate(float xStart, float yTop, float time)
+    {
+        Accelerate AccelInstance = gameObject.AddComponent<Accelerate>();
+
+        movement.acceleration.Add(AccelInstance);
+        int i = movement.acceleration.IndexOf(AccelInstance);
+
+        //Some fields
+        float x = 0;
+        float half = time / 2;
+
+        float a = (half - xStart) * (half - time);
+        float aFactor = yTop / a;
+
+        float t = 0;
+        //Move while time is still below 1
+        while (t < 1)
         {
 
-            movement.isMoving = true;
+            t += Time.deltaTime / time;
+            i = movement.acceleration.IndexOf(AccelInstance);
+            x = Mathf.Lerp(xStart, time, t);
 
-            float t = 0;
-            //Move while time is still below 1
-            while (t < 1)
-            {
-                t += Time.deltaTime / time;
-                toMove.gameObject.transform.position = Vector3.Lerp(PointA, PointB, t);
+            movement.acceleration[i].acceleration = (aFactor * ((x) * (x - time)));
 
+            yield return 0;
 
-                yield return 0;
-
-            }
-
-            //Reset Routine
-            movement.isMoving = false;
-            
         }
+      
+        //Reset Routine
+        movement.acceleration.Remove(AccelInstance);
+        Destroy(AccelInstance);
+
     }
 
     public IEnumerator RotateTowardsClosestWayPoint(Vector3 PointA, Vector3 PointB, float time, GameObject toMove)
@@ -49,21 +62,21 @@ public class CameraMovement : MonoBehaviour
 
 
 
-            
+
             while (t < 1)
             {
                 t += Time.deltaTime / time;
 
                 //Check A is positive or negative, change B to be positive or negative if A is negative/positive.
-               
+
                 Vector3 normalized = new Vector3(
                     Mathf.LerpAngle(PointA.x, PointB.x, t),
                     Mathf.LerpAngle(PointA.y, PointB.y, t),
                     Mathf.LerpAngle(PointA.z, PointB.z, t));
-                
+
                 toMove.gameObject.transform.eulerAngles = normalized;
 
-                
+
 
                 yield return 0;
 
