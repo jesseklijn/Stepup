5cc1b1d4f489b68e793ddfcabf26ac62400ce1dd
Atakan Kaya <demertknight@gmail.com>
2018-01-07 19:42:19 +0900

    Added new scene with longer woodsupport till the end + Finish.

---
+++ UnityProject/Assets/FinishScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			gameObject.GetComponentInChildren<ParticleSystem>().Play();
+			Singleton.audioController.PlaySFX("Yay", gameObject, false, false);
+		}
+	}
+	
+}
