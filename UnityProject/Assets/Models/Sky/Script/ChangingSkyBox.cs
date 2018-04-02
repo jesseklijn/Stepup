2627397fcc6422743ceb0cf5e893ba1b31401a60
Shunsuke Ishimoto <cupinosuke3mimute@gmail.com>
2018-04-02 17:34:16 +0900

    Add Dandelion par and two scripts(SkyChangeEvent and ChanginSkyBox). These are used on SkyChangeEvent.

---
+++ UnityProject/Assets/Models/Sky/Script/ChangingSkyBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingSkyBox : MonoBehaviour {
+    public bool Changing = false;
+    float SkyboxBlend = 0;
+
+    void Start () {
+        RenderSettings.skybox.SetFloat("_Blend", 0);
+    }
+	
+	
+	void Update () {
+        if (Changing == false)
+        {
+            return;
+        }
+
+        SkyboxBlend += 0.1f;
+        RenderSettings.skybox.SetFloat("_Blend", SkyboxBlend);
+        if (SkyboxBlend > 1.0f)
+        {
+            Changing = false;
+        }
+    }
+}
---
+++ UnityProject/Assets/Models/Sky/Script/SkyChangeEvent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class SkyChangeEvent : MonoBehaviour {
+    public GameObject Player;
+    public float ChangePosZ;
+    public GameObject Dandelion;
+    private ParticleSystem _Dandelion;
+    bool Passing = false;
+
+    public GameObject ChangingSkyBox;
+    private ChangingSkyBox _ChangingSkyBox;
+
+    //public Camera MainCam;
+    public GameObject MainCam;
+    private PostProcessingBehaviour behavior;
+    private DepthOfFieldModel.Settings depthsetting;
+
+	void Start () {
+        _ChangingSkyBox = ChangingSkyBox.GetComponent<ChangingSkyBox>();
+        _Dandelion = Dandelion.GetComponent<ParticleSystem>();
+
+        behavior = MainCam.GetComponent<PostProcessingBehaviour>();
+        depthsetting = behavior.profile.depthOfField.settings;
+        depthsetting.focusDistance = 4.2f;
+        behavior.profile.depthOfField.settings = depthsetting;
+    }
+	
+	
+	void Update () {
+        
+        if (Passing)
+        {
+            return;
+        }
+
+        if (Player.transform.position.z > ChangePosZ)
+        {
+            ChangeEvent();
+        }
+	}
+
+    void ChangeEvent()
+    {
+        //_Dandelion.Play();
+        _ChangingSkyBox.Changing = true;
+        Dandelion.SetActive(true);
+        depthsetting.focusDistance = 6.0f;
+        behavior.profile.depthOfField.settings = depthsetting;
+        //_Dandelion.Play();
+        Debug.Log(_Dandelion.isPlaying);
+        Passing = true;
+    }
+    
+}
