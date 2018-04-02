b87c650bbb23508d950b6ba1d0876f8d3f896f6f
Shunsuke Ishimoto <cupinosuke3mimute@gmail.com>
2018-04-02 17:50:40 +0900

    Add Fire Ball Falling Event.

---
+++ UnityProject/Assets/Models/Sky/Script/FireBallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallController : MonoBehaviour {
+
+    public bool Dead = false;
+    public float speed;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        if (gameObject.transform.position.y < -100)
+        {
+            Dead = true;
+            return;
+        }
+        gameObject.transform.position += new Vector3(speed / 3, -speed, -speed / 3);
+	}
+}
