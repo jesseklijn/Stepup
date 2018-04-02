ceb3f3a4de1ba2f5d0c79ca5080f7a039ab67735
Shunsuke Ishimoto <cupinosuke3mimute@gmail.com>
2018-04-02 17:46:01 +0900

    Add RainPar and ThunderPar.

---
+++ UnityProject/Assets/Models/Sky/Script/ThunderController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderController : MonoBehaviour {
+    public GameObject Player;
+    public GameObject ThunderA, ThunderB;
+    private ParticleSystem _ThunderA, _ThunderB;
+    private bool Passing = false;
+
+    void Start () {
+        _ThunderA = ThunderA.GetComponent<ParticleSystem>();
+        _ThunderB = ThunderB.GetComponent<ParticleSystem>();
+	}
+	
+	
+	void Update () {
+        if (Passing)
+        {
+            return;
+        }
+
+
+        if (Player.transform.position.z > 25)
+        {
+            _ThunderA.Play();
+            _ThunderB.Play();
+            Passing = true;
+        }
+    }
+}
