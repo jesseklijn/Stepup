f29b9f7dfd66e1a48392cfcb948ecf329476a947
Shunsuke Ishimoto <cupinosuke3mimute@gmail.com>
2018-04-02 17:57:27 +0900

    Add Bird and Fenix.

---
+++ UnityProject/Assets/Models/Sky/Script/BirdsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsController : MonoBehaviour {
+    public GameObject Player;
+    Animator BirdsAnimator;
+    bool Passing = false;
+    public GameObject[] feathers;
+    ParticleSystem[] _feathers;
+
+    void Start () {
+        BirdsAnimator = gameObject.GetComponent<Animator>();
+        _feathers = new ParticleSystem[feathers.Length];
+        for(int i = 0; i < feathers.Length;i++)
+        {
+            _feathers[i] = feathers[i].GetComponent<ParticleSystem>();
+        }
+    }
+	
+	
+	void Update () {
+        if (Passing)
+        {
+            return;
+        }
+
+        if (Player.transform.position.z > 135)
+        {
+            BirdsAnimator.SetTrigger("Fly");           
+            Passing = false;
+        }
+    }
+
+    void PlayFeather()
+    {
+        for (int j = 0; j < _feathers.Length; j++)
+        {
+            _feathers[j].Play();
+        }
+    }
+}
---
+++ UnityProject/Assets/Models/Sky/Script/GoalGateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalGateController : MonoBehaviour {
+    public GameObject Center, Glow, Fire;
+    private ParticleSystem _Center, _Glow, _Fire;
+
+    void Start () {
+        _Center = Center.GetComponent<ParticleSystem>();
+        _Glow = Glow.GetComponent<ParticleSystem>();
+        _Fire = Fire.GetComponent<ParticleSystem>();
+    }
+	
+	
+	void Update () {
+        
+	}
+
+    public void FirePlay()
+    {
+        _Fire.Play();
+    }
+
+    public void CenterPlay()
+    {
+        _Center.Play();
+        _Glow.Play();
+    }
+}
