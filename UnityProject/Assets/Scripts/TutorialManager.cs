47983ee2a8e88fde309c1339c5fd7ea50c414089
Atakan Kaya <demertknight@gmail.com>
2018-07-25 14:39:43 +0200

    Tutorial Scene added (prototype)

---
+++ UnityProject/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour 
+{
+	Animator anim;
+
+	void Start()
+    {
+        anim = this.GetComponent<Animator>();  
+		anim.SetTrigger("Ride");
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+        {    
+			StartCoroutine("RightStep");      
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+        { 
+			StartCoroutine("LeftStep");
+        }
+
+    }
+
+    public IEnumerator RightStep()
+    {
+		Debug.Log("Stepped Right");
+        anim.SetBool("RightStep", true);
+        anim.SetBool("LeftStep", false);
+
+        yield return new WaitForSeconds(0.05f);
+    }
+
+    public IEnumerator LeftStep()
+    {
+		Debug.Log("Stepped Left");
+        anim.SetBool("LeftStep",true);
+        anim.SetBool("RightStep", false);
+
+        yield return new WaitForSeconds(0.05f);
+    }
+}
