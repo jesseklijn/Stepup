fd3fa4294166907d5f6f0bfb5d8f42ef6bd53650
Jesse Klijn <jesse.klijn@hva.nl>
2018-08-08 17:07:12 +0100

    Stageselect finished

--- UnityProject/Assets/Scripts/StageSelector.cs
+++ UnityProject/Assets/Scripts/StageSelector.cs
@@ -7,14 +7,25 @@ using UnityEngine.UI;
 public class StageSelector : MonoBehaviour
 {
 
-    public Image[] images;
-    public string[] sceneNames;
-    private int currentStageSelect = 0;
+    public GameObject[] difficultySelectorGameObjects;
+    public GameObject[] minuteSelectorGameObjects;
 
+    public Image[] images; //difficulty
+    public Image[] minuteImages; //Minutes
+    public string[] sceneNames;
+    public int currentDifficultySelect = 0;
+    public int currentMinuteSelect = 0;
+
+    public bool minuteSelectorIsActive = false;
+    public bool difficultySelectorIsActive = true;
+
+    public AudioSource select, click, finalClick;
     // Use this for initialization
     void Start()
     {
-        ChangeAlpha(currentStageSelect);
+        ChangeDifficultyAlpha(currentDifficultySelect);
+        ChangeMinuteAlpha(currentMinuteSelect);
+        SetSelector(false);
     }
 
     // Update is called once per frame
@@ -23,49 +34,167 @@ public class StageSelector : MonoBehaviour
         //higherlevel to lowerlevel
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (ChangeCurrentSelect(currentStageSelect, -1) == true)
+            if (difficultySelectorIsActive == true)
             {
-                ChangeAlpha(currentStageSelect);
-                //TODO:playsound
+                if (ChangeCurrentDifficultySelect(currentDifficultySelect, -1) == true)
+                {
+                    ChangeDifficultyAlpha(currentDifficultySelect);
+                    //Playsound
+                    select.Play();
+                }
             }
-           
+            else if (minuteSelectorIsActive == true)
+            {
+                if (ChangeCurrentMinuteSelect(currentMinuteSelect, -1) == true)
+                {
+                    ChangeMinuteAlpha(currentMinuteSelect);
+                    //Playsound
+                    select.Play();
+                }
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (ChangeCurrentSelect(currentStageSelect, 1) == true)
+            if (difficultySelectorIsActive == true)
             {
-                ChangeAlpha(currentStageSelect);
-                //TODO:playsound
+                if (ChangeCurrentDifficultySelect(currentDifficultySelect, 1) == true)
+                {
+                    ChangeDifficultyAlpha(currentDifficultySelect);
+                    //Playsound
+                    select.Play();
+                }
+            }
+            else if (minuteSelectorIsActive == true)
+            {
+                if (ChangeCurrentMinuteSelect(currentMinuteSelect, 1) == true)
+                {
+                    ChangeMinuteAlpha(currentMinuteSelect);
+                    //Playsound
+                    select.Play();
+                }
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            GoToRandomStage();
-        }
+            //Continue
+            if (difficultySelectorIsActive == true)
+            {
+                SetSelector(true);
+                difficultySelectorIsActive = false;
+                minuteSelectorIsActive = true;
+                //Playsound
+               click.Play();
+                return;
+            }
 
+            if (minuteSelectorIsActive == true)
+            {
+
+                //Playsound
+                finalClick.Play();
+                GoToRandomStage();
+            }
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (minuteSelectorIsActive == true)
+            {
+                SetSelector(false);
+                currentMinuteSelect = 0;
+                minuteSelectorIsActive = false;
+                difficultySelectorIsActive = true;
+                return;
+            }
+
+            if (difficultySelectorIsActive == true)
+            {
+                
+                currentDifficultySelect = 0;
+                ChangeDifficultyAlpha(currentDifficultySelect);
+            }
+            
+        }
     }
 
+
+    void SetSelector(bool b)
+    {
+        if (b == true)
+        {
+            //Setactive minute selector
+            DifficultySelectorActive(false);
+            MinuteSelectorActive(true);
+
+        }
+        else
+        {
+            //Setactive Difficulty selector
+            MinuteSelectorActive(false);
+            DifficultySelectorActive(true);
+        }
+    }
+
+    void MinuteSelectorActive(bool b)
+    {
+        if (b == true)
+        {
+            for (int i = 0; i < minuteSelectorGameObjects.Length; i++)
+            {
+                minuteSelectorGameObjects[i].SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < minuteSelectorGameObjects.Length; i++)
+            {
+                minuteSelectorGameObjects[i].SetActive(false);
+            }
+        }
+    }
+
+    void DifficultySelectorActive(bool b)
+    {
+        if (b == true)
+        {
+            
+            for (int i = 0; i < difficultySelectorGameObjects.Length; i++)
+            {
+                difficultySelectorGameObjects[i].SetActive(true);
+            }
+        }
+        else
+        {
+          
+            for (int i = 0; i < difficultySelectorGameObjects.Length; i++)
+            {
+                difficultySelectorGameObjects[i].SetActive(false);
+            }
+        }
+    }
     void GoToStage(int i)
     {
-       SceneManager.LoadScene(sceneNames[i]);
+        SceneManager.LoadScene(sceneNames[i]);
     }
     void GoToRandomStage()
     {
-        SceneManager.LoadScene(sceneNames[Random.Range(0,sceneNames.Length-1)]);
+        SceneManager.LoadScene(sceneNames[Random.Range(0, sceneNames.Length - 1)]);
     }
-    bool ChangeCurrentSelect(int i, int increment)
+    bool ChangeCurrentDifficultySelect(int i, int increment)
     {
         if (i + increment <= images.Length - 1 && i + increment >= 0)
         {
-            currentStageSelect += increment;
+            currentDifficultySelect += increment;
             return true;
         }
 
         return false;
     }
-    void ChangeAlpha(int i)
+    void ChangeDifficultyAlpha(int i)
     {
 
         for (int j = 0; j < images.Length; j++)
@@ -77,6 +206,33 @@ public class StageSelector : MonoBehaviour
             else
             {
                 images[j].color = new Color(images[j].color.r, images[j].color.g, images[j].color.b, 0.5F);
+            }
+        }
+
+    }
+    bool ChangeCurrentMinuteSelect(int i, int increment)
+    {
+        if (i + increment <= minuteImages.Length - 1 && i + increment >= 0)
+        {
+            Debug.Log("+-");
+            currentMinuteSelect += increment;
+            return true;
+        }
+
+        return false;
+    }
+    void ChangeMinuteAlpha(int i)
+    {
+
+        for (int j = 0; j < minuteImages.Length; j++)
+        {
+            if (i == j)
+            {
+                minuteImages[j].color = new Color(minuteImages[j].color.r, minuteImages[j].color.g, minuteImages[j].color.b, 1);
+            }
+            else
+            {
+                minuteImages[j].color = new Color(minuteImages[j].color.r, minuteImages[j].color.g, minuteImages[j].color.b, 0.5F);
             }
         }
 
