315b27bc3271c6138343a07fc074f3d58a7ffcc5
Atakan Kaya <demertknight@gmail.com>
2018-09-29 17:20:07 +0900

    Switch motion control bug fixed.

--- UnityProject/Assets/Scripts/JoyconTest.cs
+++ UnityProject/Assets/Scripts/JoyconTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 //using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,10 +35,23 @@ public class JoyconTest : MonoBehaviour {
     bool ReachPoint_R;
     bool ReachPoint_L;
 
+    Animator rAnim, lAnim;
+    public bool rCoolingDown, lCoolingDown;
+
+    public bool testing;
+    
+
 
     // Use this for initialization
     void Start()
     {
+        _attackSE = gameObject.GetComponent<AudioSource>();
+
+        rCoolingDown = false;
+        lCoolingDown = false;
+
+        rAnim = Hand_R.GetComponent<Animator>();
+        lAnim = Hand_L.GetComponent<Animator>();
 
         //Search Switch Joycon and info
         m_joycons = JoyconManager.Instance.j;
@@ -53,201 +67,80 @@ public class JoyconTest : MonoBehaviour {
             Debug.Log("No Connect Joy-Con");
             return;
         }
-
-        ReachPoint_R = false;
-        ReachPoint_L = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SmoothMove_Hand = MoveSpeed * Time.deltaTime;
-        Hand_Timer += Time.deltaTime;
+        if(!testing)
+        {
+            if(m_joycons == null || m_joycons.Count <= 0) 
+            {
+                Debug.LogError("NO JOYCONS CONNECTED! Switching to Testing Mode");
+                testing = true;
+            }
+        }
 
-        if (m_joycons == null || m_joycons.Count <= 0) return;
+        if(!testing)
+        {
+            Vector3 JoyConL_Accel = m_joyconL.GetAccel();
+            Vector3 JoyConR_Accel = m_joyconR.GetAccel();
+            
+            if (JoyConR_Accel.magnitude >= Accel || Input.GetKeyDown(KeyCode.J))
+            {
+                PlayJoyconHand_R();
+            }
+            else if (JoyConL_Accel.magnitude >= Accel|| Input.GetKeyDown(KeyCode.F)) 
+            {
+                PlayJoyconHand_L();
+            }
+        }
 
-        //GetOrientation and InputJoycon
-        Quaternion JoyConL_Orient = m_joyconL.GetVector();
-        Quaternion JoyConR_Orient = m_joyconR.GetVector();
-        Hand_L.transform.rotation = new Quaternion(0, 0, JoyConL_Orient.x, JoyConL_Orient.w);
-        Hand_R.transform.rotation = new Quaternion(0, 0, -JoyConR_Orient.x, JoyConR_Orient.w);
-
-        Vector3 JoyConL_Accel = m_joyconL.GetAccel();
-        Vector3 JoyConR_Accel = m_joyconR.GetAccel();
-        if (JoyConR_Accel.magnitude >= Accel)
+        if (Input.GetKeyDown(KeyCode.H))
         {
             PlayJoyconHand_R();
         }
-        else if (JoyConL_Accel.magnitude >= Accel) 
+        else if (Input.GetKeyDown(KeyCode.G)) 
         {
             PlayJoyconHand_L();
         }
 
-        BackHands();
-
-
-    }
-
-    void BackHands()
-    {
-        if (ReachPoint_L)
-        {
-            var posL = Hand_L.transform.position;
-            Hand_L.transform.position = Vector3.MoveTowards(posL, HandBackPositionL.transform.position, SmoothMove_Hand);
-            Hand_L.transform.localScale = new Vector3(1f, 1f, 1f) * SmoothMove_Hand;
-            if (Vector3.Distance(posL, HandBackPositionL.transform.position) < TargetToDistance)
-            {
-                ReachPoint_L = false;
-            }
-        }
-
-        else if (ReachPoint_R)
-        {
-            var posR = Hand_R.transform.position;
-            Hand_R.transform.position = Vector3.MoveTowards(posR, HandBackPositionR.transform.position, SmoothMove_Hand);
-            Hand_R.transform.localScale = new Vector3(1f, 1f, 1f)* SmoothMove_Hand;
-            if (Vector3.Distance(posR, HandBackPositionR.transform.position) < TargetToDistance)
-            {
-                ReachPoint_R = false;
-            }
-        }
     }
 
     void PlayJoyconHand_R() //Move  R Hand Object
     {
-        int EnemyCount = GameObject.FindGameObjectsWithTag("EnemyObject").Length; //Count objects with tag "EnemyObject"
-
-        if (Hand_Timer >= AttackDuration)
+        if(!rCoolingDown)
         {
-            _attackSE = gameObject.GetComponent<AudioSource>();
+            rAnim.SetTrigger("punch");
             _attackSE.Play();
-            var ForwardPositionR = new Vector3(HandBackPositionR.transform.position.x, HandBackPositionR.transform.position.y, HandBackPositionR.transform.position.z + 1.5f);
-            var posR = Hand_R.transform.position;
-
-            if (EnemyCount == 0)
-            {
-                Hand_R.transform.LookAt(ForwardPositionR);
-                Hand_R.transform.localScale = new Vector3(ScaleFactor_Hand, ScaleFactor_Hand, ScaleFactor_Hand) * SmoothMove_Hand;
-                Hand_R.transform.position = Vector3.MoveTowards(posR, ForwardPositionR, SmoothMove_Hand);
-                if (Vector3.Distance(Hand_R.transform.position, ForwardPositionR) < TargetToDistance)
-                {
-                    Hand_Timer = 0.0f;
-                    ReachPoint_R = true;
-                    Debug.Log("RightPunch!");
-                }
-            }
-
-            else if (EnemyCount >= 1)
-            {
-                GameObject NearestObj_R = searchTag(Hand_R, "EnemyObject");
-                Debug.Log("R" + NearestObj_R);
-                Hand_R.transform.LookAt(NearestObj_R.transform);
-                Hand_R.transform.localScale = new Vector3(ScaleFactor_Hand, ScaleFactor_Hand, ScaleFactor_Hand) * SmoothMove_Hand;
-                float NearestDistance = Vector3.Distance(HandBackPositionR.transform.position, NearestObj_R.transform.position);
-
-                if (NearestDistance <= NonAttackRange)
-                {
-                    Hand_R.transform.position = Vector3.MoveTowards(posR, NearestObj_R.transform.position, SmoothMove_Hand);
-                    if (Vector3.Distance(Hand_R.transform.position, NearestObj_R.transform.position) < TargetToDistance)
-                    {
-                        m_joyconR.SetRumble(160, 320, 0.6f, 150);
-                        Hand_Timer = 0.0f;
-                        ReachPoint_R = true;
-                    }
-
-                }
-
-                else
-                {
-                    Hand_R.transform.position = Vector3.MoveTowards(posR, ForwardPositionR, SmoothMove_Hand);
-                    if (Vector3.Distance(Hand_R.transform.position, ForwardPositionR) < TargetToDistance)
-                    {
-                        Hand_Timer = 0.0f;
-                        ReachPoint_R = true;
-                    }
-                }
-            }
+            Debug.Log("RightPunch!");
+            StartCoroutine(rPunchCooldown());
         }
     }
 
     void PlayJoyconHand_L() //Move  R Hand Object
     {
-        int EnemyCount = GameObject.FindGameObjectsWithTag("EnemyObject").Length; //Count objects with tag "EnemyObject"
-
-        if (Hand_Timer >= AttackDuration)
+        if(!lCoolingDown)
         {
-            _attackSE = gameObject.GetComponent<AudioSource>();
+            lAnim.SetTrigger("punch");
             _attackSE.Play();
-            var ForwardPositionL = new Vector3(HandBackPositionL.transform.position.x, HandBackPositionL.transform.position.y, HandBackPositionL.transform.position.z + 1.5f);
-            var posL = Hand_L.transform.position;
-
-            if (EnemyCount == 0)
-            {
-                Hand_L.transform.LookAt(ForwardPositionL);
-                Hand_L.transform.localScale = new Vector3(ScaleFactor_Hand, ScaleFactor_Hand, ScaleFactor_Hand) * SmoothMove_Hand;
-                Hand_L.transform.position = Vector3.MoveTowards(posL, ForwardPositionL, SmoothMove_Hand);
-                if (Vector3.Distance(Hand_L.transform.position, ForwardPositionL) < TargetToDistance)
-                {
-                    Hand_Timer = 0.0f;
-                    ReachPoint_L = true;
-                    Debug.Log("LeftPunch 0!");
-                }
-            }
-
-            else if (EnemyCount >= 1)
-            {
-                GameObject NearestObj_L = searchTag(Hand_L, "EnemyObject");
-                Hand_L.transform.LookAt(NearestObj_L.transform);
-                Hand_L.transform.localScale = new Vector3(ScaleFactor_Hand, ScaleFactor_Hand, ScaleFactor_Hand) * SmoothMove_Hand;
-                float NearestDistance = Vector3.Distance(HandBackPositionL.transform.position, NearestObj_L.transform.position);
-
-                if (NearestDistance <= NonAttackRange)
-                {
-                    Hand_L.transform.position = Vector3.MoveTowards(posL, NearestObj_L.transform.position, SmoothMove_Hand);
-                    if (Vector3.Distance(Hand_L.transform.position, NearestObj_L.transform.position) < TargetToDistance)
-                    {
-                        m_joyconL.SetRumble(160, 320, 0.6f, 150);
-                        Hand_Timer = 0.0f;
-                        ReachPoint_L = true;
-                        Debug.Log("LeftPunch 1!");
-                    }
-
-                }
-
-                else
-                {
-                    Hand_L.transform.position = Vector3.MoveTowards(posL, ForwardPositionL, SmoothMove_Hand);
-                    if (Vector3.Distance(Hand_L.transform.position, ForwardPositionL) < TargetToDistance)
-                    {
-                        Hand_Timer = 0.0f;
-                        ReachPoint_L = true;
-                        Debug.Log("LeftPunch 2!");
-                    }
-                }
-            }
+            Debug.Log("LeftPunch!");
+            StartCoroutine(lPunchCooldown());
         }
     }
 
-
-    GameObject searchTag(GameObject nowObj, string tagName) //search nearest Object with tag
+    IEnumerator rPunchCooldown()
     {
-        float tmpDis = 0;
-        float nearDis = 0;
-        GameObject targetObj = null;
-        //Tag Retrieve the specified object as an array
-        foreach (GameObject obs in GameObject.FindGameObjectsWithTag(tagName))
-        {
-            //Get the distance between this itself and the acquired object
-            tmpDis = Vector3.Distance(obs.transform.position, nowObj.transform.position);
+        rCoolingDown = true;
+        yield return new WaitForSeconds(0.75f);
+        rCoolingDown = false;
+    }
 
-            if (nearDis == 0 || nearDis > tmpDis)
-            {
-                nearDis = tmpDis;
-                targetObj = obs;
-            }
-        }
-        return targetObj; //Get the closest object in the tag
-
+    IEnumerator lPunchCooldown()
+    {
+        lCoolingDown = true;
+        yield return new WaitForSeconds(0.75f);
+        lCoolingDown = false;
     }
 }
 
